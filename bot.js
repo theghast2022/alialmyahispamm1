ac50a3bd4d44c923be7dd233372e1dbb83abea27
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-11-21 22:46:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "9";
 
 client.on('ready', () => {
   console.log('Logged in as ${client.user.tag}!');
@@ -15,20 +14,5 @@ var channel = "510887418874953739";//ايدي الروم
     },305);
 })
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
 
 client.login(process.env.BOT_TOKEN);
